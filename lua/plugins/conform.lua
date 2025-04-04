f64aea89c145de5433c597f35c097a268a2e9da7
Radoslaw Sniezek <radoslaw.sniezek@proton.me>
2025-04-04 10:47:34 +0200

    add conform formatters

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        cpp = { 'clang-format' },
+        c = { 'clang-format' },
+        cmake = { 'cmake_format' },
+        json = { "jq" },
+      },
+    },
+  },
+}
