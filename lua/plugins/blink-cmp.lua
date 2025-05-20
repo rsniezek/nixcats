6684356033bb20994683717249b22ab716ef510d
Radoslaw Sniezek <radoslaw.sniezek@proton.me>
2025-05-20 12:00:45 +0200

    experiment with blink config

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,13 @@
+return {
+  'saghen/blink.cmp',
+  opts = {
+    completion = {
+      list = {
+        selection = {
+          preselect = true,
+          auto_insert = false,
+        },
+      },
+    },
+  },
+}
